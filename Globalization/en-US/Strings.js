a7a89c28127359b55fb9be1a70f458f1615e054c
Salvador Parra Rosas <Salvador Parra Rosas>
2017-04-13 15:27:51 -0500

    Commit inicial

---
+++ Globalization/en-US/Strings.js
@@ -0,0 +1,59 @@
+var iasp_DeleteConfirmation = "Are you sure you want to delete this record?\n\nClick OK to confirm...";
+
+var iasp_DeleteTag = "Are you sure you want to delete this tag?\n\nNOTE: This will delete the tag globally from all content.\n\n\Click OK to confirm...";
+
+var iasp_AjaxLoadingText = "<div class=\'AjaxLoader\'><div class=\'AjaxLoadingText\'>Loading...</div></div>";
+
+var ikb_strLogoutAlertText = "Are you sure you wish to logout?\n\nWhen you logout if you have chosen\nautomated login your cookie will be deleted.\n\nClick OK to confirm this action...";
+
+var ikb_strPleaseWait = "Please Wait...";
+
+var ikb_strRateCommentsSaved = "Thank you. Your comment rating has been added!";
+
+var ikb_strRateCommentsError = "Thank you. Your comment rating has been updated.";
+
+var ikb_strDefaultNodeText = "New Category";
+
+var ikb_strDefaultTroubleshooterNodeText = "New Troubleshooter Step";
+
+var ikb_strProblemSavingCategory = "There was a problem saving the category!";
+
+var ikb_strDeleteCategory = "Are you sure you want to delete this category?\n\nThis will also delete all child categories. Content within these categories will not be deleted.\n\nClick OK to delete this category...";
+
+var ikb_strDeleteTroubleshooterStep = "Are you sure you want to delete this trouble shooter step?\n\nClick OK to delete this troubleshooter step...";
+
+var ikb_strProblemDeletingCategory = "There was a problem deleting this category!";
+
+var ikb_strEmailRequired = "You must provide a valid email address to continue.";
+
+var ikb_strSendProblem = "Sorry there was a problem sending this email! Please ensure you've provided a valid email address.";
+
+var ikb_strArticleSentSuccess = "Thank you! This article has been sent successfully.";
+
+var ikb_strDeleteAttachmentConfirmation = "Are you sure you want to delete this attachment?\n\n\Click OK to confirm...";
+
+var ikb_strDeleteItemConfirmation = "Are you sure you want to delete the selected items?\n\n\Click OK to confirm...";
+
+var ikb_strDeleteTabConfirmation = "Are you sure you want to delete the selected tabs?\n\nWARNING: This will also delete all content within the selected tabs.\n\n\Click OK to confirm...";
+
+var ikb_strDeleteTabMveConfirmation = "Are you sure you want to delete the selected tabs?\n\nCategories, Custom fields & Workflows associated with content within the deleted tabs will be lost when moving content between tabs.\n\n\Click OK to confirm...";
+
+var ikb_strNoNewTabSelected = "You must select a tab to move content into from the dleted tabs!";
+
+var ikb_strDeleteConfentConfirmation = "Are you sure you want to delete this content? All comments, attachments & tags will also be removed.\n\n\Click OK to confirm...";
+
+var ikb_strAddStandardReplyProblem = "A problem occurred assign the selected standard reply.";
+
+var ikb_strLiveResultsHeader = "You may find the following helpful...";
+
+var ikb_DeleteTicket = "Are you sure you want to delete this ticket?\n\nClick OK to confirm..."
+
+var ikb_DeleteTicketReply = "Are you sure you want to delete this ticket reply?\n\nClick OK to confirm..."
+
+var ibk_Rank = "([0]% Relevancy)";
+
+var ibk_OpenInNewWindow = "Open In New Window";
+
+var ikb_RelateError = "Problem adding related content. A network problem may have occurred or you are trying to relate content to it's self.";
+
+var ikb_DeleteLinkBack = "Delete the link back to the content you are editing?"
